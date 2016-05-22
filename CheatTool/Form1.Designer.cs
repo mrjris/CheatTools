0322147a0747272fb83843902227d546bbbc602d
mrjris <mr.jris@yahoo.com>
2016-05-22 18:04:25 +0700

    Sửa lỗi chính tả.

--- CheatTool/Form1.Designer.cs
+++ CheatTool/Form1.Designer.cs
@@ -66,7 +66,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(112, 92);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Contigous Allcation";
+            this.button1.Text = "Contigous Allocation";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
